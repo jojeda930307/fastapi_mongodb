b60831f8713cee3826ea782de16556ea1e2b931c
javier <jojeda9303@gmail.com>
2022-03-26 00:02:15 +0100

    Second commit on develop

---
+++ src/routes/orders/orders_routes.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter
+
+from src.models.model import Order, OrderOut
+import src.database as db
+from src.temp.aggregations import usr_addrs, unwind_user_address, remove_addr_id, order_prod, unwind_order_prod, \
+    remove_prod_id
+
+order = APIRouter()
+
+
+@order.get('/getOrders', response_model=list[OrderOut])
+async def list_orders():
+    """ Devuelve todos los pedidos """
+
+    orders = []
+    for order in db.mydb.orders.find():
+        orders.append(Order(**order))
+    return {'orders': orders}
+
+
+@order.get("/order/{order_id}")
+def get_order_by_id(order_id: str, user_id: str):
+    """ Devuelve un pedido por su ID """
+
+    pipeline1 = [
+        usr_addrs,
+        unwind_user_address,
+        remove_addr_id
+    ]
+
+    for it1 in db.mydb.users.aggregate(pipeline1):
+        if str(it1.get('_id')) == user_id:
+            it1.pop('_id')
+
+    pipeline2 = [
+        order_prod,
+        unwind_order_prod,
+        remove_prod_id
+    ]
+
+    for it2 in db.mydb.orders.aggregate(pipeline2):
+        if str(it2.get('_id')) == order_id:
+            it2.pop('_id')
+            it2.update({'user': it1})
+            print(it2)
---
+++ src/routes/products/products_routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from src.models.model import Product
+import src.database as db
+
+product = APIRouter()
+
+
+@product.get('/getProducts')
+async def list_products():
+    """ Devuelve todos los productos """
+
+    products = []
+    for product in db.mydb.products.find():
+        products.append(Product(**product))
+    return {'product': products}
